8fe0406c0d469921bd9d318ab67cd89208f47655
Edgar A. Dikenge <dikengeofficial@gmail.com>
2024-09-03 19:43:22 +0100

    feat: done controller

--- src/main/java/api/reactive/controller/UserController.java
+++ src/main/java/api/reactive/controller/UserController.java
@@ -1,9 +1,49 @@
 package api.reactive.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import api.reactive.model.User;
+import api.reactive.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+    @GetMapping
+    public Flux<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<User>> getUserById(@PathVariable Long id) {
+        return userService.findById(id)
+                .map(user -> ResponseEntity.ok(user))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<User> createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<User>> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user)
+                .map(updatedUser -> ResponseEntity.ok(updatedUser))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deleteUser(@PathVariable Long id) {
+        return userService.delete(id)
+                .map(r -> ResponseEntity.ok().<Void>build())
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 }
